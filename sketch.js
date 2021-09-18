c96c6d98a6d96b2a113e346497505c38f15b02db
nehavinod7d <88573323+nehavinod7d@users.noreply.github.com>
2021-09-18 14:55:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var Neha,Nihar;
+function setup() {
+  createCanvas(400,400);
+Neha = new Student("Neha Vinod",13,8)
+Neha.display();
+Nihar = new Student("Nihar Vinod",9.5,4)
+Nihar.display();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
